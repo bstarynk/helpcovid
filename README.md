368b8f70086d7b9cf1327924672909efb550d4b1
Basile Starynkevitch <basile@starynkevitch.net>
2020-03-21 08:39:12 +0100

    adding empty main

---
+++ README.md
@@ -0,0 +1,48 @@
+# helpcovid
+
+A quick and dirty C++17 application (GPLv3+) to help neighbours making
+essential buying (food, medicine) during Covid epidemics. For Linux
+only. [GCC](http://gcc.gnu.org/) compiled. On [https://github.com/bstarynk/helpcovid/](helpcovid)
+
+Of course that application stores personal data in some
+[PostGreSQL](http://postgresql.org/) database, so is concerned by
+[GDPR](https://en.wikipedia.org/wiki/General_Data_Protection_Regulation)
+... It is expected to be used in good faith.
+
+If [RefPerSys](http://refpersys.org/) was ready, it should have been
+used instead.
+
+
+## license
+
+Of course [GPLv3+](https://www.gnu.org/licenses/gpl-3.0.en.html)
+
+You take the responsability about personal data issues.
+
+## contributors
+
+* [Basile Starynkevitch](http://starynkevitch.net/Basile/), near [Paris](https://en.wikipedia.org/wiki/Paris), France; email `basile-freelance@starynkevitch.net` 
+
+* [Abhishek Chakravarti](http://taranjali.org/aboutme), near [Kolkota](https://en.wikipedia.org/wiki/Kolkata), India. email `abhishek@taranjali.org` 
+
+
+## dependencies
+
+* [libonion](https://www.coralbits.com/libonion) for the Web interface
+
+* [libpq](https://www.postgresql.org/docs/11/libpq.html) from PostGreSQL 11 for the database.
+
+* [libpqxx](http://pqxx.org/development/libpqxx) for C++ frontend to PostGreSQL.
+
+On Debian/Buster `sudo aptitude install postgresql-server-dev-11 postgresql-client-11 postgresql-11 libpqxx-dev`, but `libonion` needs to be built from source
+
+
+## conventions
+
+The web root is `webroot/` or the `$HELPCOVID_WEBROOT` environment variable.
+
+The PostGreSQL connection URI is the `$HELPCOVID_POSTGRESQL` environment variable
+
+## building
+
+Edit the ̀Makefile` then run `make`
---
+++ hcv_header.hh
@@ -0,0 +1,76 @@
+/****************************************************************
+ * file hcv_header.hh
+ *
+ * Description:
+ *      Header file of https://github.com/bstarynk/helpcovid
+ *
+ * Author(s):
+ *      © Copyright 2020 
+ *      Basile Starynkevitch <basile@starynkevitch.net>
+ *      Abhishek Chakravarti <abhishek@taranjali.org>
+ *
+ *
+ * License:
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+#ifndef HELPCOVID_HEADER
+#define HELPCOVID_HEADER 1
+
+#include <set>
+#include <map>
+#include <deque>
+#include <variant>
+#include <unordered_map>
+#include <unordered_set>
+#include <new>
+#include <random>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <limits>
+#include <initializer_list>
+#include <algorithm>
+#include <mutex>
+#include <shared_mutex>
+#include <thread>
+#include <condition_variable>
+#include <atomic>
+#include <stdexcept>
+#include <functional>
+#include <typeinfo>
+#include <locale>
+
+#include <cassert>
+#include <cstring>
+#include <cmath>
+#include <cstdio>
+#include <clocale>
+
+#include <argp.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+#include <sys/syslog.h>
+#include <signal.h>
+#include <errno.h>
+#include <time.h>
+#include <dlfcn.h>
+#include <dirent.h>
+#include <pthread.h>
+
+#endif /*HELPCOVID_HEADER*/
