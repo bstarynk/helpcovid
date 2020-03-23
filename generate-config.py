40ab6dcf5eae5b115cf4b2365ebb584ab333b22f
Basile Starynkevitch <basile@starynkevitch.net>
2020-03-23 09:19:27 +0100

    adding GPLv3 notice to ./generate-config.py

--- generate-config.py
+++ generate-config.py
@@ -1,5 +1,31 @@
-#!/usr/bin/python
-
+#!/usr/bin/python3
+##***************************************************************
+## file generate-config.py
+##
+## Description:
+##      Configuration script for https://github.com/bstarynk/helpcovid
+##
+## Author(s):
+##      © Copyright 2020
+##      Basile Starynkevitch <basile@starynkevitch.net>
+##      Abhishek Chakravarti <abhishek@taranjali.org>
+##      Nimesh Neema <nimeshneema@gmail.com>
+##
+##
+## License:
+##    This HELPCOVID program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##****************************************************************************
 import os
 
 print('Starting HelpCovid configuration generator...')
