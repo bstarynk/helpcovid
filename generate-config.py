cfd7e104cf9f3b7889c5f432ef94ed4f427714a1
Abhishek Chakravarti <abhishek@taranjali.org>
2020-03-23 13:51:51 +0530

    Add postgresql group in config generator
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- generate-config.py
+++ generate-config.py
@@ -9,6 +9,7 @@ url = input('URL: ')
 root = input('Root: ')
 ssl_cert = input('SSL Certificate: ')
 ssl_key = input('SSL Key: ')
+conn_str = input('Connection String: ')
 
 # Create $HOME/.helpcovidrc file
 rc_path = os.path.expanduser('~') + '/.helpcovidrc'
@@ -19,7 +20,11 @@ rc_file.write('[web]\n\n')
 rc_file.write('url=%s\n' % url)
 rc_file.write('root=%s\n' % root)
 rc_file.write('sslcert=%s\n' % ssl_cert)
-rc_file.write('sslkey=%s\n', ssl_key)
+rc_file.write('sslkey=%s\n\n' % ssl_key)
+
+# Write keys for postgresql group
+rc_file.write('[postgresql]\n\n')
+rc_file.write('connection=%s\n' % conn_str)
 
 # Wrap up
 rc_file.close()
