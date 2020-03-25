6bd3153f881f4cbefbf7941bb31ae04884bcd53f
Basile Starynkevitch <basile@starynkevitch.net>
2020-03-25 10:32:42 +0100

    the webroot should end with a slash; fatal error when it does not

--- hcv_web.cc
+++ hcv_web.cc
@@ -67,6 +67,11 @@ void hcv_initialize_web(const std::string&weburl, const std::string&webroot, con
     HCV_FATALOUT("hcv_initialize_web: missing weburl");
   if (webroot.empty())
     HCV_FATALOUT("hcv_initialize_web: missing webroot");
+  if (webroot[webroot.size()-1] != '/')
+    HCV_FATALOUT("hcv_initialize_web: webroot directory " << webroot
+		 << " does not end with a terminating slash" <<std::endl
+		 << "... but it should." << std::endl
+		 << " For example something like /var/helpcovid/webroot/");
   if (!opensslcert.empty() && !opensslkey.empty())
     {
       struct stat certstat, keystat;
