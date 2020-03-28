ecc0451fe970478e79183381f04a3ddda7db42d6
Abhishek Chakravarti <abhishek@taranjali.org>
2020-03-28 13:45:35 +0530

    Remove redundant Hcv_LoginView class
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- hcv_views.cc
+++ hcv_views.cc
@@ -34,56 +34,25 @@ extern "C" const char hcv_views_gitid[] = HELPCOVID_GITID;
 extern "C" const char hcv_views_date[] = __DATE__;
 
 
-class Hcv_LoginView
-{
-public:
-  Hcv_LoginView(const httplib::Request& req, const httplib::Response& resp)
-    : m_req(req), m_resp(resp)
-  { }
-
-  ~Hcv_LoginView()
-  { }
-
-  std::string get()
-  {
-    Hcv_http_template_data data(m_req, m_resp, hcv_get_web_request_counter());
-    std::string thtml = hcv_get_web_root() + "html/signin.html";
-
-    return hcv_expand_template_file(thtml, &data);
-  }
-
-  std::string post()
-  {
-    // TODO: user authentication needs to be implemented.
-
-    Hcv_http_template_data data(m_req, m_resp, hcv_get_web_request_counter());
-    std::string thtml = hcv_get_web_root() + "html/index.html";
-
-#warning we need to set a cookie. See https://en.wikipedia.org/wiki/HTTP_cookie
-    return hcv_expand_template_file(thtml, &data);
-  }
-
-private:
-  httplib::Request m_req;
-  httplib::Response m_resp;
-};				// end of class Hcv_LoginView
-
-
-
-
 std::string
 hcv_login_view_get(const httplib::Request& req, httplib::Response& resp)
 {
-  Hcv_LoginView vw(req, resp);
-  return vw.get();
+  Hcv_http_template_data data(req, resp, hcv_get_web_request_counter());
+  std::string thtml = hcv_get_web_root() + "html/signin.html";
+
+  return hcv_expand_template_file(thtml, &data);
 } // end hcv_login_view_get
 
 
 std::string
 hcv_login_view_post(const httplib::Request& req,  httplib::Response& resp)
 {
-  Hcv_LoginView vw(req, resp);
-  return vw.post();
+  // TODO: user authentication and cookie setting needs to be implemented.
+  //
+  Hcv_http_template_data data(req, resp, hcv_get_web_request_counter());
+  std::string thtml = hcv_get_web_root() + "html/index.html";
+  
+  return hcv_expand_template_file(thtml, &data);
 } // end hcv_login_view_post
 
 
