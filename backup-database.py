e243fe4dd560351372b7088860a2e7076fc62921
Abhishek Chakravarti <abhishek@taranjali.org>
2020-04-02 12:48:22 +0530

    Locate config file in backup script
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- backup-database.py
+++ backup-database.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 
+
+import os
 import pwd
+
 
 
 def check_postgres_exists():
@@ -14,6 +17,24 @@ def check_postgres_exists():
 
 
 
+def find_config_file():
+    path = os.path.expanduser('~') + '/.helpcovidrc'
+    if os.access(path, os.F_OK):
+        print("Reading configuration file at " + path + "...")
+        return path
+
+    print("Configuration file not found at " + path + "...")
+   
+    while True:
+        path = input("Enter configuration file path: ")
+        if os.access(path, os.F_OK):
+            print("Reading configuration file at " + path + "...")
+            return path
+
+        print("Configuration file not found at " + path + "...")
+
+
+
 def main():
     print("Starting HelpCovid database backup...")
     print("See https://github.com/bstarynk/helpcovid and its README.md\n")
@@ -21,6 +42,8 @@ def main():
     if check_postgres_exists == False:
         print("PostgreSql database server not available; skipping...")
         return
+
+    rc_path = find_config_file()
 
 
 if __name__ == '__main__':
