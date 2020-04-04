8ca75a3bd53672e1af6b608113062671ca1c2178
Abhishek Chakravarti <abhishek@taranjali.org>
2020-04-04 15:50:46 +0530

    Attempt to load map tiles (not working)
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- webroot/js/profile.js
+++ webroot/js/profile.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
-    alert("hi!");
     var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([51.5, -0.09]).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
 });
 
