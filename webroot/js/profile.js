b074b6b110937caee0b5b850bf879598d9af47bd
Abhishek Chakravarti <abhishek@taranjali.org>
2020-04-04 15:52:55 +0530

    Successfully loaded map layer
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- webroot/js/profile.js
+++ webroot/js/profile.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
     
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    }).addTo(mymap);
 
-    L.marker([51.5, -0.09]).addTo(map)
+    L.marker([51.5, -0.09]).addTo(mymap)
         .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
         .openPopup();
 });
